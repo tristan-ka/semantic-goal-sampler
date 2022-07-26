bbf298c501b33b799cc1cb5c163bdb31478769e1
tristan-ka <tristan.karch@gmail.com>
2022-07-26 11:37:19 +0200

    new prompting strategy

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import random
 import torch
+import re
 from omegaconf import DictConfig
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
@@ -11,54 +12,71 @@ from src.playground_env.env_params import get_env_params
 from src.playground_env.descriptions import generate_all_descriptions
 
 
-def generate_prompt(known_goals):
+def generate_prompt(known_goals, prompt_type='open'):
     prompt = 'Here is a list of goals that you know:'
-    r_idx = random.choices([i for i in range(len(known_goals))],k=40)
+    r_idx = random.choices([i for i in range(len(known_goals))],k=50)
     for goal in [known_goals[i] for i in r_idx]:
         prompt += goal + ', '
-    prompt += 'what goals could you imagine from this list that is not in the list?'
+    if prompt_type == 'open':
+        prompt += 'what goals could you imagine from this list that is not in the list?'
+    elif prompt_type == 'predicate':
+        predicates = ['grow', 'grasp']
+        prompt += 'what else can you ' + random.choice(predicates) + '?'
+    else:
+        raise ValueError('Please provide a valid prompt_type')
     return prompt
 
-def encode_prompt(prompt, model, tokenizer):
+
+def encode_prompt(prompt, tokenizer):
     output = tokenizer.encode(prompt, return_tensors="pt")[0]
-    # todo: max_seq_len = max_position_embeddings -2 because of sos and eos
-    #  this is really ugly, why can't I have access to max_seq_length directly
-    # is it an input parameter to the model
-    max_len = 1024#model.config.encoder.max_position_embeddings -2
+    max_len = 1024
 
     if len(output) > max_len:
         return output[:max_len].reshape([1, max_len])
     else:
         return output.reshape([1, len(output)])
 
+def prune_output(output_text):
+    # TODO: adhoc function based on observed generated text
+    # final version should look into env grammar and remove all non environmental tokens
+    return re.sub('<.*?>', '', output_text)[3:]
+
+def write_set_to_txt(filename, out_set):
+    with open(filename, 'w') as fp:
+        for item in out_set:
+            fp.write("%s\n" % item)
+
+
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg: DictConfig) -> None:
 
-    device = "cpu"#"cuda:0" if torch.cuda.is_available() else "cpu"
+    device = cfg.device
     logging.info('Device is: ' + device)
     model_path = cfg.llm_model_path
     env_params = get_env_params()
+    prompt_type = cfg.prompt_type
+
+    logging.info(prompt_type)
     train_descriptions, test_descriptions, extra_descriptions = generate_all_descriptions(env_params)
 
-    
-    # model_path = 'model_files/roberta2gpt2-daily-dialog'
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_path).to(device)
 
-    output_text = []
-    for tries in range(100):
-        prompt = generate_prompt(train_descriptions)
-        inputs = encode_prompt(prompt, model, tokenizer).to(device)
+    goal_candidates = []
+    for tries in range(cfg.n_gen):
+        prompt = generate_prompt(train_descriptions, prompt_type=prompt_type)
+        inputs = encode_prompt(prompt, tokenizer).to(device)
         outputs = model.generate(inputs)
-        output_text.append(tokenizer.decode(outputs[0]))
+        goal_candidate = prune_output(tokenizer.decode(outputs[0]))
+        goal_candidates.append(goal_candidate)
 
-    logging.info(tokenizer.decode(outputs[0]))
-    with open(r'output_goals.txt', 'w') as fp:
-        for item in output_text:
-            # write each item on a new line
-            fp.write("%s\n" % item)
-            logging.info(item)
-        
+    set_candidates = set(goal_candidates)
+    set_train = set(train_descriptions)
+    set_test = set(test_descriptions)
+
+    write_set_to_txt(r'candidates_U_test.txt', set_candidates.intersection(set_test))
+    write_set_to_txt(r'candidates_U_train.txt', set_candidates.intersection(set_train))
+    write_set_to_txt(r'others.txt', set_candidates - set_train - set_test)
 
 if __name__ == '__main__':
     main()
